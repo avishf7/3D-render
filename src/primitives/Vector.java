3d044fc3f251ac5ad72293696a81fa06f457503a
avishay farkash <62483176+avishf7@users.noreply.github.com>
2021-03-08 17:19:36 +0200

    vector V.1

--- src/primitives/Vector.java
+++ src/primitives/Vector.java
@@ -1,12 +1,30 @@
 package primitives;
 
 public class Vector {
+
+	public Vector(Coordinate x, Coordinate y, Coordinate z) {
+		Point3D tmp = new Point3D(x,y,z);
+		if(tmp.equals(Point3D.ZERO))
+			throw new IllegalArgumentException("Invalid vector - (0,0,0)");
+		head = tmp;
+	}
+/**
+ * 
+ * @param x
+ * @param y
+ * @param z
+ */
 	public Vector(double x, double y, double z) {
-		
+		Point3D tmp = new Point3D(x,y,z);
+		if(tmp.equals(Point3D.ZERO))
+			throw new IllegalArgumentException("Invalid vector - (0,0,0)");
+		head = tmp;
 	}
 
-	public Vector(Point3D head) {
-		this.head = head;
+	public Vector(Point3D other) {
+		if(other.equals(Point3D.ZERO))
+			throw new IllegalArgumentException("Invalid vector - (0,0,0)");
+		this.head = other;
 	}
 
 	Point3D head;
