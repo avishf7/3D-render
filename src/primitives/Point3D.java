eabc836dd3341e51389a41e879d2ec292182c9e9
שי אקסלרוד <Saxelrod@g.jct.ac.il>
2021-03-10 15:32:04 +0200

    Documentation V.1.1

--- src/primitives/Point3D.java
+++ src/primitives/Point3D.java
@@ -2,35 +2,79 @@ package primitives;
 
 import java.util.Objects;
 
+/**
+ * Class Point3D is the basic class representing a point in the
+ * Three-dimensional space.
+ * 
+ * @author Shai&Avishay
+ */
 public class Point3D {
-
+	/**
+	 * A constant static variable that represents the "zero point"
+	 */
 	public static final Point3D ZERO = new Point3D(0, 0, 0);
 
+	/**
+	 * Point3D constructor receiving coordinate values.
+	 * @param x value of the Coordinate that relative to the x-axis
+	 * @param y value of the Coordinate that relative to the y-axis
+	 * @param z value of the Coordinate that relative to the z-axis
+	 */
 	public Point3D(double x, double y, double z) {
 		this.x = new Coordinate(x);
 		this.y = new Coordinate(y);
 		this.z = new Coordinate(z);
 	}
 
+	/**
+	 * Point3D constructor receiving coordinates.
+	 * @param x Coordinate relative to the x-axis
+	 * @param y Coordinate relative to the y-axis
+	 * @param z Coordinate relative to the z-axis
+	 */
 	public Point3D(Coordinate x, Coordinate y, Coordinate z) {
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 
+	/**
+	 * Coordinate relative to the x-axis
+	 */
 	final Coordinate x;
+	/**
+	 * Coordinate relative to the y-axis
+	 */
 	final Coordinate y;
+	/**
+	 * Coordinate relative to the z-axis
+	 */
 	final Coordinate z;
 
+	/**
+	 * 
+	 * @param point
+	 * @return
+	 */
 	public Vector subtract(Point3D point) {
 		return new Vector(x.coord - point.x.coord, y.coord - point.y.coord, z.coord - point.z.coord);
 	}
 
+	/**
+	 * 
+	 * @param vec
+	 * @return
+	 */
 	public Point3D add(Vector vec) {
 		return new Point3D(x.coord + vec.getHead().x.coord, y.coord + vec.getHead().y.coord,
 				z.coord + vec.getHead().z.coord);
 	}
 
+	/**
+	 * 
+	 * @param point
+	 * @return
+	 */
 	public double distanceSquared(Point3D point) {
 
 		return (x.coord - point.x.coord) * (x.coord - point.x.coord)
@@ -38,6 +82,11 @@ public class Point3D {
 				+ (z.coord - point.z.coord) * (z.coord - point.z.coord);
 	}
 
+	/**
+	 * 
+	 * @param point
+	 * @return
+	 */
 	public double distance(Point3D point) {
 		return Math.sqrt(distanceSquared(point));
 	}
