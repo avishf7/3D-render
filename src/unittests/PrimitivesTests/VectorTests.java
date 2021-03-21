6331443553e1bd628ab7e506ba9fa65eda94f87c
avishay farkash <62483176+avishf7@users.noreply.github.com>
2021-03-21 20:31:20 +0200

    unittests Documentation V.1.2

--- src/unittests/PrimitivesTests/VectorTests.java
+++ src/unittests/PrimitivesTests/VectorTests.java
@@ -19,10 +19,6 @@ import primitives.Vector;
  */
 public class VectorTests {
 
-	private Vector v1 = new Vector(1, 2, 3);
-	private Vector v2 = new Vector(-2, -4, -6);
-	private Vector v3 = new Vector(0, 3, -2);
-
 	/**
 	 * Test method for
 	 * {@link primitives.Vector#Vector(primitives.Coordinate, primitives.Coordinate, primitives.Coordinate)}.
@@ -54,29 +50,12 @@ public class VectorTests {
 	}
 
 	/**
-	 * Test method for {@link primitives.Vector#add(primitives.Vector)}.
-	 */
-	@Test
-	public void testAdd() {
-		// TC01: Test Vector + Vector
-		assertTrue("Vector + Vector does not work correctly", new Vector(-1, -2, -3).equals(v1.add(v2)));
-
-	}
-
-	/**
-	 * Test method for {@link primitives.Vector#subtract(primitives.Vector)}.
-	 */
-	@Test
-	public void testSubtract() {
-		// TC01: Test Vector - Vector
-		assertTrue("Vector - Vector does not work correctly", new Vector(-3, -6, -9).equals(v2.subtract(v1)));
-	}
-
-	/**
 	 * Test method for {@link primitives.Vector#scale(double)}.
 	 */
 	@Test
 	public void testScale() {
+		Vector v1 = new Vector(1, 2, 3);
+
 		// TC01: Test if Scale product work correctly
 		assertTrue("Scale product does not work correctly", new Vector(2, 4, 6).equals(v1.scale(2)));
 	}
@@ -86,9 +65,13 @@ public class VectorTests {
 	 */
 	@Test
 	public void testDotProduct() {
+		Vector v1 = new Vector(1, 2, 3);
+		Vector v2 = new Vector(-2, -4, -6);
+		Vector v3 = new Vector(0, 3, -2);
+
 		// TC01: Test if dotProduct() result for orthogonal vectors is zero.
 		assertTrue("dotProduct() for orthogonal vectors is not zero", isZero(v1.dotProduct(v3)));
-		
+
 		// TC02: Test if dotProduct() result is correct
 		assertTrue("dotProduct() wrong value", isZero(v1.dotProduct(v2) + 28));
 
@@ -99,6 +82,10 @@ public class VectorTests {
 	 */
 	@Test
 	public void testCrossProduct() {
+		Vector v1 = new Vector(1, 2, 3);
+		Vector v2 = new Vector(-2, -4, -6);
+		Vector v3 = new Vector(0, 3, -2);
+		
 		// TC01: Test Cross-Product between parallel vectors
 		assertThrows("crossProduct() for parallel vectors does not throw an exception", IllegalArgumentException.class,
 				() -> v1.crossProduct(v2));
@@ -106,7 +93,7 @@ public class VectorTests {
 		Vector vr = v1.crossProduct(v3);
 		// TC02: Test Cross-Product vs vector length
 		assertTrue("crossProduct() wrong result length", isZero(vr.length() - v1.length() * v3.length()));
-		
+
 		// TC03: Test if Cross-Product result is orthogonal to the multiplied vectors
 		assertTrue("crossProduct() result is not orthogonal to its operands",
 				isZero(vr.dotProduct(v1)) || !isZero(vr.dotProduct(v3)));
@@ -135,12 +122,14 @@ public class VectorTests {
 	 */
 	@Test
 	public void testNormalize() {
+		Vector v1 = new Vector(1, 2, 3);
+		
 		Vector vCopy = new Vector(v1.getHead());
 		Vector vCopyNormalize = vCopy.normalize();
-		
+
 		// TC01: Test Test if normalize() function does not create a new vector
 		assertTrue("normalize() function creates a new vector", vCopy == vCopyNormalize);
-		
+
 		// TC02: Test vector normalization vs vector length
 		assertTrue("normalize() result is not a unit vector", isZero(vCopyNormalize.length() - 1));
 	}
@@ -150,6 +139,8 @@ public class VectorTests {
 	 */
 	@Test
 	public void testNormalized() {
+		Vector v1 = new Vector(1, 2, 3);
+		
 		// TC01: Test if normalized() function create a new vector
 		Vector u = v1.normalized();
 		assertTrue("normalized() function does not create a new vector", u != v1);
