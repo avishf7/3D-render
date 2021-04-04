fabb8d882c99ef084944662d1d719bb0d59dc444
שי אקסלרוד <Saxelrod@g.jct.ac.il>
2021-04-04 12:18:56 +0300

    Geometries

---
+++ src/geometries/Geometries.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package geometries;
+
+import java.util.List;
+
+import primitives.Point3D;
+import primitives.Ray;
+
+/**
+ * @author User
+ *
+ */
+public class Geometries implements Intersectable {
+
+	List<Intersectable> shapes;
+	@Override
+	public List<Point3D> findIntsersections(Ray ray) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
