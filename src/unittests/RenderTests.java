b2a6abf280f0d98a7191ab4ca9b17dec56479db0
שי אקסלרוד <Saxelrod@g.jct.ac.il>
2021-05-03 14:44:17 +0300

    push

--- src/unittests/RenderTests.java
+++ src/unittests/RenderTests.java
@@ -53,24 +53,20 @@ public class RenderTests {
 	/**
 	 * Test for XML based scene - for bonus
 	 */
-	/*@Test
-	
-	public void basicRenderXml() {
-		Scene scene = new Scene("XML Test scene");
-		// enter XML file name and parse from XML file into scene object
-		// ...
-
-		ImageWriter imageWriter = new ImageWriter("xml render test", 1000, 1000);
-		Render render = new Render() //
-				.setImageWriter(imageWriter) //
-				.setScene(scene) //
-				.setCam(camera) //
-				.setRayTracer(new RayTracerBasic(scene));
-
-		render.renderImage();
-		render.printGrid(100, new Color(java.awt.Color.YELLOW));
-		render.writeToImage();
-	}*/
+	/*
+	 * @Test
+	 * 
+	 * public void basicRenderXml() { Scene scene = new Scene("XML Test scene"); //
+	 * enter XML file name and parse from XML file into scene object // ...
+	 * 
+	 * ImageWriter imageWriter = new ImageWriter("xml render test", 1000, 1000);
+	 * Render render = new Render() // .setImageWriter(imageWriter) //
+	 * .setScene(scene) // .setCam(camera) // .setRayTracer(new
+	 * RayTracerBasic(scene));
+	 * 
+	 * render.renderImage(); render.printGrid(100, new
+	 * Color(java.awt.Color.YELLOW)); render.writeToImage(); }
+	 */
 
 	@Test
 	public void basicRenderMultiColorTest() {
@@ -78,12 +74,16 @@ public class RenderTests {
 				.setAmbientLight(new AmbientLight(new Color(java.awt.Color.WHITE), 0.2)); //
 
 		scene.geometries.add(new Sphere(new Point3D(0, 0, -100), 50), //
-				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, 100, -100), new Point3D(-100, 100, -100)) // up left
+				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, 100, -100), new Point3D(-100, 100, -100)) // up
+																													// left
 						.setEmmission(new Color(java.awt.Color.GREEN)),
-				new Triangle(new Point3D(100, 0, -100), new Point3D(0, 100, -100), new Point3D(100, 100, -100)), // up right
-				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, -100, -100), new Point3D(-100, -100, -100)) // down left
+				new Triangle(new Point3D(100, 0, -100), new Point3D(0, 100, -100), new Point3D(100, 100, -100)), // up
+																													// right
+				new Triangle(new Point3D(-100, 0, -100), new Point3D(0, -100, -100), new Point3D(-100, -100, -100)) // down
+																													// left
 						.setEmmission(new Color(java.awt.Color.RED)),
-				new Triangle(new Point3D(100, 0, -100), new Point3D(0, -100, -100), new Point3D(100, -100, -100)) // down right
+				new Triangle(new Point3D(100, 0, -100), new Point3D(0, -100, -100), new Point3D(100, -100, -100)) // down
+																													// right
 						.setEmmission(new Color(java.awt.Color.BLUE)));
 
 		ImageWriter imageWriter = new ImageWriter("color render test", 1000, 1000);
